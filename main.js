a616fc23d26d19757c9444cbe617a43a42012c30
John Fredy <john@JohnMorales>
2022-11-20 22:16:41 +1100

    fix route

--- main.js
+++ main.js
@@ -7,12 +7,18 @@ var router = express.Router();
 require("dotenv").config();
 
 const app = express();
-app.set("views", "views");
+app.set('views', path.join(__dirname, '..', 'views'));
 app.set("view engine", "ejs");
 app.use(cors({ origin: "*" }));
 app.use(bodyparser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, '..', 'views', 'layouts'),
+    partialsDir: path.join(__dirname, '..', 'views', 'partials')}
+));
 
 app.get("/", (req, res) => {
     getData(
